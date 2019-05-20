0bab4a0dde981eb9bf0fb21bfc2bb971efb729ed
Christian Meunier <chris@jumpn.com>
2019-05-20 23:59:30 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# CockroachDB Ecto Adaptor
+
+This is a very small fork of the official Postgresql Adaptor bundled with `ecto_sql`.
+
+The main reason for this fork is Ecto 3.x no longer keeps microseconds information for `:time`, `:naive_datetime` and `:utc_datetime`.
+
+The net effect is it uses timestamp(0) when creating fields with such types and the migration table used by Ecto is using one of those types.
+
+The problem is CockroachDB does not support timestamp with precision unless it's the default precision (6).
+
+This adaptor use the default precision for those types.
+It also disable the migration lock given locking is not supported by CockroachDB either.
+
+For more information, check this github issue: https://github.com/cockroachdb/cockroach/issues/32098
+
+## Installation
+
+The package can be installed
+by adding `ecto_cockroachdb` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:ecto_cockroachdb, "~> 1.0.0"}
+  ]
+end
+```
---
+++ mix.exs
@@ -0,0 +1,51 @@
+defmodule EctoCockroachDB.MixProject do
+  use Mix.Project
+
+  @version "1.0.0"
+
+  def project do
+    [
+      app: :ecto_cockroachdb,
+      version: @version,
+      elixir: "~> 1.5",
+      start_permanent: Mix.env() == :prod,
+      deps: deps(),
+      name: "CockroachDB Ecto Adaptor",
+      description: "CockroachDB adaptor for Ecto",
+      source_url: "https://github.com/jumpn/ecto_cockroachdb",
+      docs: docs(),
+      package: package()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme"
+    ]
+  end
+
+  defp deps do
+    [
+      {:ex_doc, "~> 0.20", only: :docs},
+      {:ecto, "~> 3.1"},
+      {:ecto_sql, "~> 3.1"},
+      {:postgrex, ">= 0.14.3"}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Christian Meunier"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/jumpn/ecto_cockroachdb"}
+    ]
+  end
+end
